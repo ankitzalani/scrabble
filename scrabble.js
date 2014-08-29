f4c9a0bcd4da8d817d7c5363a87fdd52637e3921
Ankit Zalani <ankit.littlegenius@gmail.com>
2014-08-29 07:38:15 -0700

    Added fix for generating minified js.

--- scrabble.js
+++ scrabble.js
@@ -2,6 +2,10 @@
   var scrabble = {
     version: "1.0.00"
   };
-  if (typeof define === "function" && define.amd) define(scrabble); else if (typeof module === "object" && module.exports) module.exports = scrabble;
-  this.scrabble = scrabble;
+  if (typeof define === "function" && define.amd) {
+    define(scrabble);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = scrabble;
+    this.scrabble = scrabble;
+  }
 }();
