8715985b9a3a8505af8a7e40a8ec56a6be58de1e
Ankit Zalani <ankit.littlegenius@gmail.com>
2014-10-23 06:47:50 -0700

    Removing unnecessary changes

--- scrabble.js
+++ scrabble.js
@@ -2,10 +2,4 @@
   var scrabble = {
     version: "1.0.00"
   };
-  if (typeof define === "function" && define.amd) {
-    define(scrabble);
-  } else if (typeof module === "object" && module.exports) {
-    module.exports = scrabble;
-    this.scrabble = scrabble;
-  }
 }();
--- src/end.js
+++ src/end.js
@@ -1,8 +1,2 @@
-if (typeof define === "function" && define.amd) {
-	define(scrabble);
-} else if (typeof module === "object" && module.exports) {
-	module.exports = scrabble;
-	this.scrabble = scrabble;
-}
 
 }();
